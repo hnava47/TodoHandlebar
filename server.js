61201bf226f1592b120b5bdde4dd784a52c0dfd9
Hector Nava <hector.r.nava44@gmail.com>
2022-02-03 11:34:40 -0700

    Added functions for get all and create user

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config');
-// const routes = require('./routes');
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
