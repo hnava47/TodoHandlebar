eef922f61f2af32e36a4ab283d85f1e014395338
Hector Nava <hector.r.nava44@gmail.com>
2022-02-03 07:53:31 -0700

    Added handlebar, body parser, and listening for port on server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config');
+// const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const User = require('./models/User');
+const hbs = exphbs.create({});
+
+// Tells node we're using handlebars as our template engine
+// configured handlebars as the view engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+});
