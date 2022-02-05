56d2317e234460ff0a860b464fd29587e100586f
Hector Nava <hector.r.nava44@gmail.com>
2022-02-05 10:40:20 -0700

    Added paths npm package

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config');
