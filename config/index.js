1541cc2726aa89ddd357bdfb7fb0ec1605d044a0
Hector Nava <hector.r.nava44@gmail.com>
2022-02-05 10:24:11 -0700

    Updated database to run on Heroku

--- config/index.js
+++ config/index.js
@@ -1,15 +1,24 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    }
-);
+let sequelize;
+// This will only run in production
+// this environmental variable only exists in production
+if (process.env.JAWSDB_URL) {
+    // Variable only exists in Heroku
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // Connect to database locally if not in production/Heroku
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
 
 module.exports = sequelize;
