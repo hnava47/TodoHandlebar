c64ed0fe1dca5146205183c78eea7bde7858625d
Hector Nava <hector.r.nava44@gmail.com>
2022-02-05 13:26:44 -0700

    Added app.js login function

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
-console.log('hello');
+const $email = document.querySelector('#email');
+const $password = document.querySelector('#password');
+const $login = document.querySelector('#login');
+
+$login.addEventListener('click', async function(event) {
+    event.preventDefault();
+    try {
+        const response = await fetch('/api/users/login' {
+            method: 'POST',
+            body: JSON.stringify({
+                email: $email.value,
+                password: $password.value
+            })
+        });
+
+        const data = await response.json();
+        window.location.reload();
+    } catch (e) {
+        console.log(e);
+    }
+});
