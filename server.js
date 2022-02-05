139dbddd1728184ae767c9ee1711a116c95c9fc1
Hector Nava <hector.r.nava44@gmail.com>
2022-02-05 13:18:28 -0700

    Added public folder

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sessionSettings));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
